0de33c63cf6a202d12bd90b12e4a7043f66fe6ca
xxBennny <benpro100@gmail.com>
2025-12-28 15:21:15 -0600

    add icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import Head from 'next/head';
 
 type RootLayoutProps = {
   children: React.ReactNode;
@@ -7,6 +8,22 @@ type RootLayoutProps = {
 
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => (
   <html lang='en'>
+    <Head>
+      <title>Ben Edgar-Prosen</title>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link
+        rel="icon"
+        href="/icon.png"
+        type="image/png"
+        sizes="any"
+      />
+      <link
+        rel="apple-touch-icon"
+        href="/apple-icon.png"
+        type="image/png"
+        sizes="any"
+      />
+    </Head>
     <body>
       {children}
       <SpeedInsights />

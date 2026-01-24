46bc237d0a37a7f07af6ef15325847b70ccf0538
bprosen <benedgarprosen@gmail.com>
2026-01-24 17:31:38 -0600

    biome

---
+++ src/constants/urls.ts
@@ -0,0 +1,3 @@
+export const GITHUB = 'https://github.com/bprosen';
+export const LINKED_IN = 'https://www.linkedin.com/in/ben-edgar-prosen/';
+export const EMAIL = 'mailto:benedgarprosen@gmail.com';
